7be026d0b659b116eff89087871acd3d369e339c
drumman22 <grossmanjason0@gmail.com>
2021-08-31 17:25:36 -0400

    Initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Application {
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("What is your name? ");
+
+        String name = in.nextLine();
+        System.out.printf("%s\n", name);
+    }
+}
